3635984fde681e0613639a2534be4032e7f54859
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-11-10 22:49:38 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+};
+let timerId = null;
+// console.log(refs.start);
+// console.log(refs.stop);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onClickStart(evt) {
+  refs.start.disabled = true;
+  timerId = setInterval(() => {
+    const bodyEl = document.querySelector('body');
+    bodyEl.style.background = getRandomHexColor();
+    console.log(bodyEl.style.background);
+  }, 1000);
+}
 
+function onClickStop(evt) {
+  refs.start.disabled = false;
+  clearInterval(timerId);
+}
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
