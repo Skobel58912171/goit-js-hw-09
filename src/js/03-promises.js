04f53f2e0ca9e4cb68357095e3cae392733fe8e6
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-11-14 02:26:26 +0300

    14.11.22

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+// console.dir(formEl)
+function onSubmit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.target.elements;
+  const amountV = Number(amount.value);
+  const delayV = Number(delay.value);
+  const stepV = Number(step.value);
+  console.log(amount.value);
+  console.log(delay.value);
+  console.log(step.value);
+  let delayTime = delayV;
+  const formBtn = formEl.lastElementChild;
+  formBtn.disabled = false;
+  for (let i = 1; i <= amountV; i += 1) {
+    createPromise(i, delayTime)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayTime += stepV;
   }
+  formBtn.disabled = true;
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Notify.success;
+        resolve({ position, delay });
+        // Fulfillres
+      } else {
+        // Notify.failure;
+        reject({ position, delay }); // Reject
+      }
+    }, delay);
+  });
 }
